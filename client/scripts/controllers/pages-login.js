87de66e4ae4473af8aa539d2cd686e8eb362843e
the1Spartacus <thulanianesh@gmail.com>
2017-09-13 10:11:22 +0200

    is running boss

--- client/scripts/controllers/pages-login.js
+++ client/scripts/controllers/pages-login.js
@@ -53,24 +53,3 @@ app
 
 
 
-//  .controller('AuthLogoutController', ['$scope', 'AuthService', '$state',
-//      function($scope, AuthService, $state) {
-//    AuthService.logout()
-//      .then(function() {
-//        $state.go('core.login');
-//      });
-//  }])
-//  .controller('SignUpController', ['$scope', 'AuthService', '$state',
-//      function($scope, AuthService, $state) {
-//    $scope.user = {
-//      email: 'baz@qux.com',
-//      password: 'bazqux'
-//    };
-//
-//    $scope.register = function() {
-//      AuthService.register($scope.user.email, $scope.user.password)
-//        .then(function() {
-//          $state.transitionTo('core.signup');
-//        });
-//    };
-//  }]);
