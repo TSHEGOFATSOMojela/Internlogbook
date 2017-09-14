d6cab2f2817c25cf54b26433eb83d8a47f24836a
TSHEGOFATSO <mojelatshego@gmail.com>
2017-09-14 11:29:36 +0200

    created http request

--- client/scripts/controllers/forms-wizard.js
+++ client/scripts/controllers/forms-wizard.js
@@ -1,12 +1,19 @@
 'use strict';
 
 //angular.module('app', ['ui.bootstrap','ui.utils']);
+//
+//app.controller('FormWizardCtrl', ['$scope', 'Company', function($scope, Company) {
+//
+//    
+//    
+//    //get all companies in the database
+//     $scope.Companies = Company.find();
+//
+//  }])
 
-app.controller('FormWizardCtrl', ['$scope', 'Company', function($scope, Company) {
 
-    
-    
-    //get all companies in the database
-     $scope.Companies = Company.find();
-
-  }])
+ app.controller('FormWizardCtrl', function($scope, $http) {
+    $http.get("http://localhost:3000/api/Companies").then(function (response) {
+        $scope.Companies = response.data;
+    });
+});
