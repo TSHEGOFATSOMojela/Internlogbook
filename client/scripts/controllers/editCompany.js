1334f3950fc896f1e505d24e051a2f2d9018657f
Lerato-Raloobo <eraloobo@gmail.com>
2017-09-15 14:04:33 +0200

    raloobe

--- client/scripts/controllers/editCompany.js
+++ client/scripts/controllers/editCompany.js
@@ -2,15 +2,10 @@
 
 //angular.module('app', ['ui.bootstrap','ui.utils']);
 
-app.controller('EditCompanyCtrl', ['$scope', '$http', function($scope, $http) {
-
-     $scope.company = {};
-    
-    $scope.editCompany=function(){
-        $http.getElementById('http://localhost:3000/api/Companies',$scope.company_id).success(function (response) ) {
-        $scope.company = response.data;
-    }
-  }
+app.controller('EditCompanyCtrl', ['$scope', '$http','viewCompany', function($scope, $http,viewCompany) {
+        $http.findById('http://localhost:3000/api/Companies').then(function (response) ) {
+          $scope.viewCompany = response.data;
+    }                                                                                                                                                                  
 
   }])
 
