584d931700e589b1a4fdc80b28682262ec91c010
the1Spartacus <thulanianesh@gmail.com>
2017-12-12 10:52:57 +0200

    dealing with objectives thulani1

--- client/scripts/controllers/tables-bootstrap.js
+++ client/scripts/controllers/tables-bootstrap.js
@@ -42,10 +42,53 @@
           .deleteById({id: $stateParams.id})
           .$promise
           .then(function() {
-            $state.go('app.shop.single-order');
+            $state.go('app.tables.bootstrap');
           });
       }else
-               $state.go('app.shop.single-order');
-          }]);
+               $state.go('app.tables.bootstrap');
+          }])
+
+.controller('OrdersCtrl', ['$scope', 'Objective', '$state',
+               function($scope, Objective,  $state) {
+
+
+
+  //create/add a new Objective
+   $scope.addObjective = function() {
+      Objective
+        .create({
+          name: $scope.newObjective.title,
+          dueDate: $scope.newObjective.dueDate,
+          desc: $scope.newObjective.description,
+          status: $scope.newObjective.status
+       }).$promise
+        .then(function() {
+          //location go to view Objective page
+          $state.go('app.tables.bootstrap');
+        });
+    };
+
+    
+  }])
+
+//edit and save objective
+.controller('EditOrdersCtrl', ['$scope','$q', 'Objective','$stateParams', '$state', function($scope, $q, Objective, $stateParams, $state) {
+    //edit Objective
+    $scope.newObjective = {};
+       Objective.findById({ id: $stateParams.id })
+          .$promise
+          .then(function(data) {
+        $scope.newObjective = data;
+   
+      });
+   //save company
+    $scope.addObjective = function() {
+      $scope.newObjective
+        .$save()
+        .then(function(newObjective) {
+          $state.go('app.tables.bootstrap');
+        });
+    };
+  }])
 
 
